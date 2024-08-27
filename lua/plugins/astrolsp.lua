b6b10668274a92150a9b4060cfb0d068b9faa795
Erik Lee <2141800+aethertap@users.noreply.github.com>
2024-08-27 10:46:27 -0500

    added special feature for rust analyzer

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+--if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroLSP allows you to customize the features in AstroNvim's LSP configuration engine
 -- Configuration documentation can be found with `:h astrolsp`
@@ -20,7 +20,7 @@ return {
     formatting = {
       -- control auto formatting on save
       format_on_save = {
-        enabled = true, -- enable or disable format on save globally
+        enabled = false, -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
           -- "go",
         },
@@ -45,6 +45,16 @@ return {
     ---@diagnostic disable: missing-fields
     config = {
       -- clangd = { capabilities = { offsetEncoding = "utf-8" } },
+      rust_analyzer = {
+        settings = {
+        ["rust-analyzer"] = {
+            cargo = {
+              features = {"nvim_analyzer"},
+              --overrideCommand = {"cargo" ,"clippy","--message-format=json", "--all-targets", "--all-features"}
+            }
+          }
+        }
+      }
     },
     -- customize how language servers are attached
     handlers = {
